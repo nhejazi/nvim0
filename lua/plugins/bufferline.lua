3b1902ab7225759caff0960f1e31470ce6094647
Nima Hejazi <nh@nimahejazi.org>
2025-01-07 18:34:52 -0500

    tweak bufferline

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,9 +1,27 @@
 return {
-  -- TODO: fix colorscheme integration
   {
     "akinsho/bufferline.nvim",
-    enabled = true,
     version = "*",
     dependencies = "nvim-tree/nvim-web-devicons",
+    opts = {
+      options = {
+        themable = true,
+        show_buffer_close_icons = false,
+        show_tab_indicators = false,
+        separator_style = "slant",
+        indicator = {
+          style = "none",
+        },
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Explorer",
+            highlight = "FileExplorer",
+            text_align = "left",
+            separator = true,
+          },
+        },
+      },
+    },
   },
 }
