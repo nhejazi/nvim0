6fda1d218d23612fb661b90ffcb8710c95ac73e4
Nima Hejazi <nh@nimahejazi.org>
2025-01-07 16:11:53 -0500

    fix markdown rendering

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,11 +1,3 @@
--- every spec file under /plugins will be loaded automatically by lazy.nvim
---
--- In your plugin files, you can:
--- * add extra plugins
--- * disable/enabled LazyVim plugins
--- * override the configuration of LazyVim plugins
-
--- adapted from https://github.com/lervag/vimtex?tab=readme-ov-file#lazynvim
 return {
   -- markdown auto-render engine
   {
@@ -18,11 +10,13 @@ return {
     opts = {
       -- options for code display
       code = {
-        style = "none",
         sign = false,
-        width = 0,
-        border = "none",
+        style = "full",
+        width = "block", -- only has an effect when style = "full"
+        left_pad = 0,
         right_pad = 4,
+        border = "thick",
+        inline_pad = 1,
       },
       -- options to always show (never hide via local conceal behavior)
       -- anti_conceal = {
@@ -32,17 +26,6 @@ return {
       --     link = true,
       --   },
       -- },
-    },
-  },
-
-  -- markdown viewer
-  {
-    "OXY2DEV/markview.nvim",
-    enabled = false,
-    lazy = false,
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "nvim-tree/nvim-web-devicons",
     },
   },
 }
